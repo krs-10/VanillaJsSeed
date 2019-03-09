effef0bb294f877d28a73188faae5112a923e210
krs-10 <kayeouh@gmail.com>
2019-03-09 15:29:38 -0800

    Updating bash scripts :)

--- existing.sh
+++ existing.sh
@@ -28,41 +28,24 @@ ask_continue () {
 }
 
 # default values
-repo_base_url=https://github.com/krs-10/VanillaJsSeed
-repo_src_url=git@github.com:krs-10/VanillaJsSeed.git
+repo_base_url="https://github.com/krs-10/VanillaJsSeed.git"
 default_repo_name="NewVanillaJsSeed"
 default_dir="../NewVanillaJsSeed"
 default_branch="master"
 
+read -rp "Enter the link to your existing Github repo (required): " args_existing_repo
+existing_repo=${args_existing_repo:?"This is a required value."}
+existing_repo=`ask_continue $existing_repo "username"`
 
-read -rp "Enter your Github username (required): " args_user_name
-user_name=${args_user_name:?"This is a required value."}
-user_name=`ask_continue $user_name "username"`
-
-echo "================================="
-
-
-read -rp "Enter the name of your new repo (default is $default_repo_name): " args_repo_name
-user_repo_name=${args_repo_name:-$default_repo_name}
-# user_repo_name=`ask_continue $user_repo_name "repo name"`
-
-echo "================================="
-
-default_dir="../$user_repo_name"
 read -rp "Enter your directory location - remember, it is relative to this folder! (default is $default_dir): " args_dir
 dir=${args_dir:-$default_dir}
 
-# dir=`ask_continue $dir "directory location"`
-
-repo="git@github.com:${user_name}/${user_repo_name}.git"
-echo $repo
-
-echo "Cloning Vanilla JS into $dir..."
+echo "Cloning Vanilla JS into $dir"
 rm -rf "$dir"
-# base_dir=${dir##*/}
 mkdir "$dir"
 cd "$dir"
-git clone --depth 1 "$repo_base_url" .
+git clone --depth 1 $repo_base_url .
+
 
 # echo "Removing old .git folder..."
 rm -rf .git
@@ -72,18 +55,15 @@ git init
 git add .
 
 # # creating an initial commit
-git commit -m "Initial Commit"
+git commit -m "Pulling in VanillaJS source"
 
 # # asking for user's branch
-read -rp "Enter your new branch: " args_branch
+read -rp "Enter your repo's target branch (default is $default_branch): " args_branch
 branch=${args_branch:-$default_branch}
-branch=`ask_continue $branch "repo branch (default is $default_branch)"`
+branch=`ask_continue $branch "repo branch"`
 
-git remote add origin $repo
+git remote add origin $existing_repo
 
-# echo "Pushing to $repo ..."
-git push -u origin $repo
+git push -u origin $branch
 
 echo "All done! :D"
-
-
--- init.sh
+++ init.sh
@@ -28,8 +28,7 @@ ask_continue () {
 }
 
 # default values
-repo_base_url=https://github.com/krs-10/VanillaJsSeed
-repo_src_url=git@github.com:krs-10/VanillaJsSeed.git
+repo_base_url="https://github.com/krs-10/VanillaJsSeed.git"
 default_repo_name="NewVanillaJsSeed"
 default_dir="../NewVanillaJsSeed"
 default_branch="master"
@@ -39,30 +38,21 @@ read -rp "Enter your Github username (required): " args_user_name
 user_name=${args_user_name:?"This is a required value."}
 user_name=`ask_continue $user_name "username"`
 
-echo "================================="
-
-
 read -rp "Enter the name of your new repo (default is $default_repo_name): " args_repo_name
 user_repo_name=${args_repo_name:-$default_repo_name}
-# user_repo_name=`ask_continue $user_repo_name "repo name"`
-
-echo "================================="
 
 default_dir="../$user_repo_name"
 read -rp "Enter your directory location - remember, it is relative to this folder! (default is $default_dir): " args_dir
 dir=${args_dir:-$default_dir}
 
-# dir=`ask_continue $dir "directory location"`
-
 repo="git@github.com:${user_name}/${user_repo_name}.git"
-echo $repo
 
-echo "Cloning Vanilla JS into $dir..."
+echo "Cloning Vanilla JS into $dir"
 rm -rf "$dir"
-# base_dir=${dir##*/}
 mkdir "$dir"
 cd "$dir"
-git clone --depth 1 "$repo_base_url" .
+git clone --depth 1 $repo_base_url .
+
 
 # echo "Removing old .git folder..."
 rm -rf .git
@@ -81,7 +71,21 @@ branch=`ask_continue $branch "repo branch (default is $default_branch)"`
 
 git remote add origin $repo
 
-# echo "Pushing to $repo ..."
-git push -u origin $repo
+echo '================================='
+echo "YOUR REPO INFO"
 
-echo "All done! :D"
+echo "Please go to https://github.com/new and enter the following info:"
+echo "*******"
+echo "Name: ${user_repo_name}"
+echo "Owner: ${user_name}"
+echo "Remote upstream: ${repo}"
+echo "*******"
+
+echo "Don't forget to clean out your package.json :)"
+echo '================================='
+
+
+# echo "Pushing to $repo ..."
+# git push -u origin $repo
+
+# echo "All done! :D"
